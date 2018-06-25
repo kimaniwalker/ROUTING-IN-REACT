10bc0f17dc03e115ead370ee9d40eb19439b8408
Kimani Walker <kimaniwalker@gmail.com>
2018-06-25 13:46:23 -0500

    Added Fragment to return

--- src/components/films/index.jsx
+++ src/components/films/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 const Base_URL = 'https://ghibliapi.herokuapp.com/films';
 
@@ -6,17 +6,21 @@ const Base_URL = 'https://ghibliapi.herokuapp.com/films';
 class Films extends Component {
     render() {
         return(
+            <Fragment>
             <h1> This is the films page</h1>
+            
+            </Fragment>
         )
     }
     
     componentDidMount() {
-        fetch(`${Base_URL}`)
+     fetch(`${Base_URL}`)
   .then(function(response) {
     return response.json();
   })
-  .then(function(myJson) {
-    console.log(myJson);
+  .then(function(list) {
+    console.log(list);
+    
   });
     }
 }
