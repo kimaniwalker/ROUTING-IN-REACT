a753c2f50f8d2d5d78e6aaa959ed8ccd8657aff3
Kimani Walker <kimaniwalker@gmail.com>
2018-06-25 13:49:24 -0500

    Added FilmsList component & PeopleList component

---
+++ src/components/filmsList/index.jsx
@@ -0,0 +1,28 @@
+import React, {Component, Fragment} from 'react';
+
+const Base_URL = 'https://ghibliapi.herokuapp.com/films';
+
+
+class FilmsList extends Component {
+    render() {
+        return(
+            <Fragment>
+            <h1> This is the films page</h1>
+            
+            </Fragment>
+        )
+    }
+    
+    componentDidMount() {
+     fetch(`${Base_URL}`)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(list) {
+    console.log(list);
+    
+  });
+    }
+}
+
+export default FilmsList;
