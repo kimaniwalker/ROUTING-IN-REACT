5284942a0fdab1647a72ed42ef69a25dcb5753f0
Kimani Walker <kimaniwalker@gmail.com>
2018-06-25 13:10:45 -0500

    Add fetch to films component

--- src/components/films/index.jsx
+++ src/components/films/index.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 
+const Base_URL = 'https://ghibliapi.herokuapp.com/films';
+
 
 class Films extends Component {
-    render () {
-        return (
-            <h1> This is the films page </h1>
+    render() {
+        return(
+            <h1> This is the films page</h1>
         )
+    }
+    
+    componentDidMount() {
+        fetch(`${Base_URL}`)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
     }
 }
 
